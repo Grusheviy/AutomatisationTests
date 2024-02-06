7612319628f991a7df9b6ccd42b68133f1c221f1
Ivan Tereshchenko <geminimzk@gmail.com>
2024-02-06 21:08:07 +0400

    Added DeliveryTest.java

--- HW_4/untitled/src/test/java/TestAddOrder/OrdersTest.java
+++ HW_4/untitled/src/test/java/TestAddOrder/OrdersTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import java.sql.SQLException;
 
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrdersTest extends AbstractTest {
 
---
+++ HW_4/untitled/src/test/java/TestDelivery/DeliveryTest.java
@@ -0,0 +1,100 @@
+package TestDelivery;
+
+import org.example.home.CustomersEntity;
+import org.example.home.DeliveryEntity;
+import org.example.home.OrdersEntity;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DeliveryTest extends AbstractTest {
+
+    @Test
+    @Order(1)
+    void addDelivery_whenValid_shouldSave() {
+        // given
+        Session session = getSession();
+        session.beginTransaction();
+
+        int maxDeliveryId = (int) session.createSQLQuery("SELECT MAX(delivery_id) FROM delivery").uniqueResult();
+        int nextDeliveryId = maxDeliveryId + 1;
+
+        int maxOrderId = (int) session.createSQLQuery("SELECT MAX(order_id) FROM orders").uniqueResult();
+
+        final Query query = session.createSQLQuery("INSERT INTO delivery(delivery_id, order_id, courier_id, date_arrived, taken, payment_method) " +
+                        "VALUES (:deliveryId, :orderId, :courierId, :dateArrived, :taken, :paymentMethod)")
+                .setParameter("deliveryId", nextDeliveryId)
+                .setParameter("orderId", maxOrderId + 1)
+                .setParameter("courierId", 1)
+                .setParameter("dateArrived", "2023-02-28 12:00:00")
+                .setParameter("taken", "No")
+                .setParameter("paymentMethod", "Cash");
+
+        // when
+        int rowsAffected = query.executeUpdate();
+
+        // then
+        session.getTransaction().commit();
+
+        Assertions.assertEquals(1, rowsAffected);
+    }
+
+    @Test
+    @Order(2)
+    void getDeliveryById_whenExists_shouldReturnDelivery() {
+        // given
+        final Query query = getSession()
+                .createSQLQuery("SELECT * FROM delivery")
+                .addEntity(DeliveryEntity.class);
+
+        // when
+        int countTableSize = query.list().size();
+
+        // then
+        Assertions.assertEquals(16, countTableSize);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"1", "10", "16"})
+    @Order(3)
+
+    void getDeliveryByCustomerId_whenValid_shouldReturn(int deliveryId) throws SQLException {
+        // given
+        final Query query = getSession()
+                .createSQLQuery("SELECT * FROM delivery WHERE delivery_id='" + deliveryId + "'")
+                .addEntity(DeliveryEntity.class);
+
+        // when
+        DeliveryEntity deliveryEntity = (DeliveryEntity) query.uniqueResult();
+
+        // then
+        Assertions.assertNotNull(deliveryEntity);
+    }
+
+    @Test
+    @Order(4)
+    void deleteDelivery_whenValid_shouldDelete() {
+        //given
+        final Query query = getSession()
+                .createSQLQuery("SELECT * FROM delivery WHERE delivery_id=" + 16).addEntity(DeliveryEntity.class);
+        Optional<DeliveryEntity> deliveryEntity = (Optional<DeliveryEntity>) query.uniqueResultOptional();
+        Assumptions.assumeTrue(deliveryEntity.isPresent());
+        //when
+        Session session = getSession();
+        session.beginTransaction();
+        session.delete(deliveryEntity.get());
+        session.getTransaction().commit();
+        //then
+        final Query queryAfterDelete = getSession()
+                .createSQLQuery("SELECT * FROM delivery WHERE delivery_id=" + 16).addEntity(DeliveryEntity.class);
+        Optional<DeliveryEntity> deliveryEntityAfterDelete = (Optional<DeliveryEntity>) queryAfterDelete.uniqueResultOptional();
+        Assertions.assertFalse(deliveryEntityAfterDelete.isPresent());
+    }
+}
